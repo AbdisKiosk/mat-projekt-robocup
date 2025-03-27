fb75b7ca7307c214e7ad60eb6c27ad6fb0dc1fc4
AbdisKiosk <85417512+AbdisKiosk@users.noreply.github.com>
2025-03-27 11:26:01 +0100

    Initial commit

---
+++ src/main/java/org/example/RetroRobot.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class RetroRobot {
+
+    private final String name;
+    private final int velocityMS;
+    private final int rotationSpeedRad;
+    private final int stopTime;
+
+}
